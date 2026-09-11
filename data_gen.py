b1d56450ec8d5889be33c98c80f68543f3b5b8f5
sharish <narayanankasharish@johndeere.com>
2026-09-11 12:03:02 +0530

    feat: wrapped point generation in a func

--- data_gen.py
+++ data_gen.py
@@ -15,43 +15,37 @@ def potential_function(k):
     
     return potential_eval, force_eval_x, force_eval_y
 
-G = 1
-M = 1
-k = G * M
-dt = 0.01
-
-x, y = 1.0, 0.0
-x_values = [x]
-y_values = [y]
-rs = [np.sqrt(x**2 + y**2)]
-vx, vy = 0.0, 0.5
 vsx = []
 vsy =[]
 E = [0]
-potential_eval, force_eval_x, force_eval_y = potential_function(k)
+dt = 0.01
+def generate_data(k=3, x=1.0, y=0.0, vx=0.0, vy=1.7, dt=0.01):
+    rs = [np.sqrt(x**2 + y**2)]
+    x_values = [x]
+    y_values = [y]
+    potential_eval, force_eval_x, force_eval_y = potential_function(k)
 
-a_x = force_eval_x(x, y)
-a_y = force_eval_y(x, y)
-for i in range(600):
-    x += vx*dt + 0.5 * a_x * dt**2
-    y += vy*dt + 0.5 * a_y * dt**2
-    ax_new = force_eval_x(x, y)
-    ay_new = force_eval_y(x, y)
-    vx += 0.5 * (a_x + ax_new) * dt
-    vy += 0.5 * (a_y + ay_new) * dt
-    a_x, a_y = ax_new, ay_new
-    r_new= np.sqrt(np.array(x)**2 + np.array(y)**2)
-    E.append(0.5 * (vx**2 + vy**2) + potential_eval(x, y))
-    x_values.append(x)
-    y_values.append(y)
-    rs.append(r_new)
-    vsx.append(vx)
-    vsy.append(vy)
-    #acx.append(ax)
-    #acy.append(ay)
+    a_x = force_eval_x(x, y)
+    a_y = force_eval_y(x, y)
+    for i in range(600):
+        x += vx*dt + 0.5 * a_x * dt**2
+        y += vy*dt + 0.5 * a_y * dt**2
+        ax_new = force_eval_x(x, y)
+        ay_new = force_eval_y(x, y)
+        vx += 0.5 * (a_x + ax_new) * dt
+        vy += 0.5 * (a_y + ay_new) * dt
+        a_x, a_y = ax_new, ay_new
+        r_new= np.sqrt(np.array(x)**2 + np.array(y)**2)
+        E.append(0.5 * (vx**2 + vy**2) + potential_eval(x, y))
+        x_values.append(x)
+        y_values.append(y)
+        rs.append(r_new)
+        vsx.append(vx)
+        vsy.append(vy)
+    return x_values, y_values, rs, vsx, vsy, E
 
 fig, axes = plt.subplots(1, 2, figsize=(12, 5))
-
+x_values, y_values, rs, vsx, vsy, E = generate_data()
 time_values = np.arange(len(rs)) * dt
 
 axes[0].plot(x_values, y_values, color='tab:blue', linewidth=2)
